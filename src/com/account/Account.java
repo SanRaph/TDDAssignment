fcfc0dadb7d2fd163792d778c840cc3da20a9368
daunikman <daunikman@gmail.com>
2020-07-19 05:18:12 +0100

    File Structure: Added .gitIgnore File to the code base Review: Added some code review comments on 2 methods and a class variable

--- src/com/account/Account.java
+++ src/com/account/Account.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class Account {
 
     private int accountBalance;
+    //@todo Mr Raph, Pin numbers are not constants, users are allowed to change their pins when they wish. Create methods for them to change their pins
     static int PIN = 1564;
 
 
@@ -19,6 +20,7 @@ public class Account {
         return accountBalance;
     }
 
+    //@todo Mr Raph, I think you should integrate the withdrw with pin and withdrw money method together, it is a security risk having them differently in my opinion or don't you think so?
     public void withdrawMoney(int withdrawalAmount) {
         if(withdrawalAmount <= accountBalance-1000){
             accountBalance -= withdrawalAmount;
@@ -36,6 +38,7 @@ public class Account {
         }
     }
 
+    //@todo Mr Raph i think i will need an explanation for this method and what it actually does, i understand the code but i need to understand the rationale behind the code
     public void wrongPinTrial(int pin){
         int count = 3;
         boolean cardLocked = false;
@@ -53,3 +56,4 @@ public class Account {
         }
     }
 }
+//@todo Nice Job so far though, see you at the Village ... Chibuzo Ekejiuba ....Semicolon Africa
